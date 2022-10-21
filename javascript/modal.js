ffe4bb4b545f110ae4c10e02fcc0f37a5b927459
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-10-21 18:20:17 +0300

    +

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,6 +1,8 @@
 const STORAGE_KEY = 'inputLocallStorageKey';
-// import Notiflix from 'notiflix';
-const refs = {
+import optionsForNotiflix from './services/notiflix-options.js';
+Notiflix.Notify.init(optionsForNotiflix);
+
+const modalWindow = {
   form: document.querySelector('.modal__form'),
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
@@ -9,210 +11,105 @@ const refs = {
   modal: document.querySelector('[data-modal]'),
   body: document.querySelector('body'),
   formData: { ...JSON.parse(localStorage.getItem(STORAGE_KEY)) },
-  // по умолчанию берет данные из локал стоража
+
   toggleModal() {
-    window.addEventListener('keydown', refs.onKeyPresEsq);
-    //при открытии модалки добавляем слушателя событий - чтобы слушать глобально (чтобы при нажатии `esc` срабатывала функция)
-    refs.modal.classList.toggle('bacdrop--is-hidden');
-    // переключаем класс
-    refs.body.classList.toggle('no-scroll');
-    // переключаем класс
+    const { modal, body, onKeyPresEsq } = this;
+    window.addEventListener('keydown', onKeyPresEsq);
+    modal.classList.toggle('bacdrop--is-hidden');
+    body.classList.toggle('no-scroll');
   },
 
   onKeyPresEsq(event) {
-    if (event.code === 'Escape') {
-      refs.modal.classList.toggle('bacdrop--is-hidden');
-      refs.body.classList.toggle('no-scroll');
-      window.removeEventListener('keydown', refs.onKeyPresEsq);
-      // убираем слушателя событий на ОКНО - чтобы слушать глобально (чтобы при нажатии ескейп НЕ РАБОТАЛА функция)
+    const { modal, body, onKeyPresEsq } = modalWindow;
+    const { code } = event;
+    if (code === 'Escape') {
+      modal.classList.toggle('bacdrop--is-hidden');
+      body.classList.toggle('no-scroll');
+      window.removeEventListener('keydown', onKeyPresEsq);
     }
   },
+
   closeOnTargetClick(event) {
-    // console.log(event.currentTarget);
-    //event.currentTarget -- текущий єлемент где висит евент листнер
-    // console.log(event.target); // используем чтобы точно указать что выбираем
-    //event.target -- целевой єлемент, куда мы в интерфейсе жмакнули
-    if (event.currentTarget === event.target) {
-      refs.toggleModal();
+    const { currentTarget, target } = event;
+
+    if (currentTarget === target) {
+      modalWindow.toggleModal();
     }
-    // event.currentTarget передает ссылку на тот элемент на котором его закрепили в addEventListener
-    // event.target === выосоточная 🚀 которая показывает ссылку на тот элент на которой мы кликнули
-    // если при клике две ссылки совпадут то вызовем колбек функцию которая переключает классы  на боди
   },
+
   onSubmit(event) {
+    const { mail, name, policy, feedback, pnone } = event.currentTarget.elements;
+    const { modal, body } = this;
     event.preventDefault();
-    //При отправке формы страница не делает перезагрузку.
     if (
-      event.currentTarget.elements.mail.value === '' ||
-      //если поле с почтой пустое или
-      event.currentTarget.elements.name.value === '' ||
-      //если поле с паролем пустое
-      !event.currentTarget.elements.policy.checked === true ||
-      //если на чекбоксе не стоит галочка
-      event.currentTarget.elements.feedback.value === '' ||
-      //если поле с коментарием пустое
-      event.currentTarget.elements.pnone.value === ''
-      //если поле телефоном пустое
+      mail.value === '' ||
+      name.value === '' ||
+      !policy.checked === true ||
+      feedback.value === '' ||
+      pnone.value === ''
     ) {
       Notiflix.Notify.failure('Внимание! Все поля должны быть заполнены.');
       return;
-      // выводим строку о проблеме
-      // не вЫполняем код ниже
     }
-
-    const formData = new FormData(event.currentTarget);
-    // собирает всю дату из полей и под капотом делает огромный итератор который жанглирует этими данными (интерфейс для данных которе там лежат)
+    const capturedData = new FormData(event.currentTarget);
     const saveData = {};
-    // делаем переменную с пустым объектом для данных которые получим от пользователя
-    formData.forEach((value, key) => {
+    capturedData.forEach((value, key) => {
       saveData[key] = value;
-      //  собираем данные в объект
-      refs.modal.classList.toggle('bacdrop--is-hidden');
-      //закрываем модалку
-      refs.body.classList.toggle('no-scroll');
-      // возвращаем скрол
+      modal.classList.toggle('bacdrop--is-hidden');
+      body.classList.toggle('no-scroll');
     });
 
     Notiflix.Notify.info('Мы собрали данные, скоро с Вами свяжиться наш менеджер');
     console.log('Мы собрали данные ==>', saveData);
-    // выводим собранные данные
     event.currentTarget.reset();
-    //делаем сброс
     localStorage.removeItem(STORAGE_KEY);
-    //очистка локального хранилища
   },
+
   onFormInput(event) {
-    refs.formData[event.target.name] = event.target.value;
-    // в refs.formData создаем ключ (event.target.name ==> в нашей feedback.formData.email или feedback.formData.message), если оно есть то не создаем + записываем значение event.target.value (то что вбиваем в поля)
-    console.log(refs.formData);
-    // console.log((refs.formData[event.target.name] = event.target.value));
-    if (refs.checkBox.checked) {
-      refs.formData.policy = 'on';
-    } else if (!refs.checkBox.checked) {
-      refs.formData.policy = 'off';
+    const { formData, checkBox } = this;
+    const { name, value } = event.target;
+    formData[name] = value;
+    console.log(formData);
+    if (checkBox.checked) {
+      formData.policy = 'on';
+    } else if (!checkBox.checked) {
+      formData.policy = 'off';
     }
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(refs.formData));
-    // переделываем в JSON {email: "value", message: "value"} ==> {"email": "value", "message": "value"} и записываем в локальное хранилище STORAGE_KEY
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.formData));
   },
 
   populateTextareaMulti() {
-    //{'mail', 'sergey@gmail.com', 'policy', 'on'...} => {mail: 'sergey@gmail.com', policy: 'on'...} => [['mail', 'sergey@gmail.com'], ['policy', 'on']...] и перебираем + записуем значения в value
-    const message = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    // возвращаем JSON в обычный объект и записываем в переменную
-    if (!message) {
+    const { form, checkBox } = this;
+    const parsedDataFromLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (!parsedDataFromLocalStorage) {
       return;
-    }
-
-    // если в localStorage нет интирисующей нас записи то заканчиваем функцию
-    if (message) {
-      for (let i of refs.form.elements) {
-        // запускаем цикл в котором перебираем по штучно каждый элемент формы
-        // console.log(i.name, i);
-        // refs.form.elements => .elements показывает все элементы + name покажет значения атрибута name
-        const entris = Object.entries(message);
-        // {mail: 'sergey@gmail.com', policy: 'on'...} => [['mail', 'sergey@gmail.com'], ['policy', 'on']...]
-        if (i.name === 'policy') {
-          // если атрибут name == policy (отлавливаем соглашщение checkbox)
+    } else if (parsedDataFromLocalStorage) {
+      for (let element of form.elements) {
+        const entris = Object.entries(parsedDataFromLocalStorage);
+        if (element.name === 'policy') {
           entris.forEach(([name, value]) => {
-            // запускае перебор forEach на подготовленном массиве с массивами [['policy', 'on']...]
             if (name === 'policy' && value === 'on') {
-              // если ['policy', 'on']
-              refs.checkBox.checked = true;
-              // ставим чекбокс активным
+              checkBox.checked = true;
             }
           });
         }
         entris.forEach(([name, value]) => {
-          // запускае перебор forEach на подготовленном массиве с массивами [['policy', 'on']...]
-          if (i.name === name) {
-            // если в эл атрибут name === name  (['mail', 'sergey@gmail.com'])
-            i.value = value;
-            //записуем значение в инпут
+          if (element.name === name) {
+            element.value = value;
           }
         });
       }
     }
   },
 };
-refs.populateTextareaMulti();
-// запускаем функцию которая заполняет поля из localStorage
+modalWindow.populateTextareaMulti();
 
-refs.openModalBtn.addEventListener('click', refs.toggleModal);
-refs.closeModalBtn.addEventListener('click', refs.toggleModal);
-refs.modal.addEventListener('click', refs.closeOnTargetClick);
-refs.form.addEventListener('submit', refs.onSubmit);
-refs.form.addEventListener('input', _.throttle(refs.onFormInput, 500));
-// запускаем слушателя событий на заполнение форм + колбек функцию + lodash.throttle (задержка 500 секунд)
-
-//настройки Notiflix
-Notiflix.Notify.init({
-  width: '280px',
-  position: 'right-bottom', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
-  distance: '10px',
-  opacity: 1,
-  borderRadius: '5px',
-  rtl: false,
-  timeout: 3000,
-  messageMaxLength: 110,
-  backOverlay: false,
-  backOverlayColor: 'rgba(0,0,0,0.5)',
-  plainText: true,
-  showOnlyTheLastOne: false,
-  clickToClose: false,
-  pauseOnHover: true,
-
-  ID: 'NotiflixNotify',
-  className: 'notiflix-notify',
-  zindex: 4001,
-  fontFamily: 'Quicksand',
-  fontSize: '17px',
-  cssAnimation: true,
-  cssAnimationDuration: 400,
-  cssAnimationStyle: 'fade' - 'zoom', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
-  closeButton: false,
-  useIcon: false,
-  useFontAwesome: true,
-  fontAwesomeIconStyle: 'basic' - 'shadow', // 'basic' - 'shadow'
-  fontAwesomeIconSize: '34px',
-
-  success: {
-    background: '#32c682',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-success',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-check-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(50,198,130,0.2)',
-  },
-
-  failure: {
-    background: '#ff5549',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-failure',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-times-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(255,85,73,0.2)',
-  },
-
-  warning: {
-    background: '#F5F4FA',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-warning',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-exclamation-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(238,191,49,0.2)',
-  },
-
-  info: {
-    background: '#2196F3',
-    textColor: '#FFFFFF',
-    childClassName: 'notiflix-notify-info',
-    notiflixIconColor: 'rgba(255,255,255)',
-    fontAwesomeClassName: 'fas fa-info-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(38,192,211,0.2)',
-  },
-});
+modalWindow.openModalBtn.addEventListener('click', modalWindow.toggleModal.bind(modalWindow));
+modalWindow.closeModalBtn.addEventListener('click', modalWindow.toggleModal.bind(modalWindow));
+modalWindow.modal.addEventListener('click', modalWindow.closeOnTargetClick.bind(modalWindow));
+modalWindow.form.addEventListener('submit', modalWindow.onSubmit.bind(modalWindow));
+modalWindow.form.addEventListener(
+  'input',
+  _.throttle(modalWindow.onFormInput.bind(modalWindow), 500)
+);
--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,111 +1,49 @@
-import products from './portfolio-data.js';
-console.log(products);
+import products from './services/portfolio-data.js';
 const source = document.getElementById('entry-template').innerHTML;
 const template = Handlebars.compile(source);
-
 const STORAGE_KEY = 'selectedFilter';
 
-const refs = {
-  btnPanel: document.querySelector('.buttons-set'),
-  portfolio: document.querySelector('.card-set'),
+const filtersOnPortfolio = {
+  btnPanelEl: document.querySelector('.buttons-set'),
+  portfolioEl: document.querySelector('.card-set'),
   filterData: JSON.parse(localStorage.getItem(STORAGE_KEY)),
-  filterChose(event) {
-    if (event.target.nodeName !== 'BUTTON') {
+
+  populateFromLocalStorage() {
+    const { filterData } = filtersOnPortfolio;
+    if (filterData) {
+      this.createMarkUp(filterData);
+      const allBtns = document.querySelectorAll('.buttons-set__button');
+      for (const Btn of allBtns) {
+        if (Btn.dataset.filter === filterData) {
+          Btn.focus();
+        }
+      }
+    } else {
       return;
     }
-    const selectedFilter = event.target.dataset.filter;
-    refs.createMarkUp(selectedFilter);
-    console.log(selectedFilter);
+  },
+
+  filterChose(event) {
+    const { nodeName, dataset } = event.target;
+    if (nodeName !== 'BUTTON') {
+      return;
+    }
+    const selectedFilter = dataset.filter;
+    this.createMarkUp(selectedFilter);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(selectedFilter));
   },
+
   createMarkUp(selectedFilter) {
+    const { portfolioEl } = this;
     const filteredArrays = products.filter(product => product.filter.includes(selectedFilter));
-    console.log(filteredArrays);
-    console.log(template(filteredArrays));
     const cardsEl = template(filteredArrays);
-
-    refs.portfolio.innerHTML = '';
-    refs.portfolio.insertAdjacentHTML('beforeend', cardsEl);
+    portfolioEl.innerHTML = '';
+    portfolioEl.insertAdjacentHTML('beforeend', cardsEl);
   },
-
-  // createMarkUp(selectedFilter) {
-  //   const filteredArrays = products.filter(product => product.filter.includes(selectedFilter));
-  //   const cardsEl = filteredArrays
-  //     .map(
-  //       ({
-  //         alt,
-  //         name,
-  //         description,
-  //         category,
-  //         filter,
-  //         generalImg,
-  //         imgDesctop,
-  //         imgTable,
-  //         imgMobile,
-  //       }) => {
-  //         return `
-  //                  <li class="card-set__item" data-filter="${filter}">
-  //                 <a
-  //                   href="https://google.com/"
-  //                   target="_blank"
-  //                   rel="noopener noreferrer nofollow"
-  //                   class="card-set__link"
-  //                 >
-  //                   <div class="graphic-content">
-  //                     <picture>
-  //                       <source
-  //                         srcset="${imgDesctop}"
-  //                         media="(min-width: 1200px)"
-  //                       />
-  //                       <source
-  //                         srcset="${imgTable}"
-  //                         media="(min-width: 768px)"
-  //                       />
-  //                       <source
-  //                         srcset="${imgMobile}"
-  //                         media="(min-width: 320px)"
-  //                       />
-  //                       <img
-  //                         class="graphic-content__img"
-  //                         src="${generalImg}"
-  //                         alt="${alt}"
-  //                         width="370"
-  //                       />
-  //                     </picture>
-  //                     <div class="overflow">
-  //                       <p class="graphic-content__img-overflow">${description}
-  //                       </p>
-  //                     </div>
-  //                   </div>
-  //                   <div class="text-content">
-  //                     <h2 class="text-content__title">${name}</h2>
-  //                     <p class="text-content__text">${category}</p>
-  //                   </div>
-  //                 </a>
-  //               </li>
-  //       `;
-  //       }
-  //     )
-  //     .join('');
-  //   refs.portfolio.innerHTML = '';
-  //   refs.portfolio.insertAdjacentHTML('beforeend', cardsEl);
-  // },
 };
 
-if (refs.filterData) {
-  // если в refs.filterData есть значения
-  refs.createMarkUp(refs.filterData);
-  // передаем его в разметку
-  const allBtns = document.querySelectorAll('.buttons-set__button');
-  // делаем переменную со всеми кнопками в панели
-  for (const Btn of allBtns) {
-    // перебираем все кнопки
-    if (Btn.dataset.filter === refs.filterData) {
-      // data-filter=".." кнопки ==== записи в refs.filterData
-      Btn.focus();
-      //делаем фокус на кнопку
-      // в css убрал на focus outline: none;
-    }
-  }
-}
-refs.btnPanel.addEventListener('click', refs.filterChose);
+filtersOnPortfolio.btnPanelEl.addEventListener(
+  'click',
+  filtersOnPortfolio.filterChose.bind(filtersOnPortfolio)
+);
+filtersOnPortfolio.populateFromLocalStorage();
